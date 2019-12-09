cf71f88ab791952bb70df0a493d043cc1ee0940b
JeffreyCroatoan23 <omnia2332@gmail.com>
2019-12-09 17:18:14 -0600

    inicio proyecto

---
+++ main.js
@@ -0,0 +1,34 @@
+//Inputs
+let name = document.getElementById("Name");
+let add = document.getElementById("Add");
+let food = document.getElementById("Food");
+//Etiquetas p
+let textName = document.getElementById("textName");
+let textAdd = document.getElementById("textAdd");
+let textFood = document.getElementById("textFood");
+//boton
+let boton = document.getElementById("boton");
+
+const ordenar = () =>{
+
+
+textName.innerHTML = name.value;
+textAdd.innerHTML = add.value;
+let resultFood = food.value.toUpperCase();
+
+    if(resultFood === "CON"){
+        textFood.innerHTML = "UmU";
+    }else if(resultFood === "SIN"){
+        textFood.innerHTML = "UwU";
+    }else{
+        textFood.innerHTML = "No te entendi";
+    }
+}
+
+
+boton.addEventListener("click", ordenar);
+
+document.getElementById("boton").addEventListener("click", function(event){
+    event.preventDefault()
+});
+
